2b6cdef4d0e0441d00ab09b4788d482969af4c87
Aliaksandr Limasau <alexandr.limasov@gmail.com>
2020-10-18 14:06:56 +0300

    git fix

--- home-work-app/src/app/app.tsx
+++ home-work-app/src/app/app.tsx
@@ -1,28 +1,25 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
 import { Routing } from './pages/routing';
 import { Footer } from './components/Footer/Footer';
 import { AppCatch } from './components/ErrorBoundaries/AppCatch/AppCatch.error';
 import { GlobalStyle } from './configs/GlobalStyle';
 import { mainTheme } from './configs/MainTheme';
-import { Provider } from 'react-redux';
-import { ThemeProvider } from 'styled-components';
 import { store } from './redux/store';
-import { MovieService } from './services/MovieService';
 
 type App = () => void;
 
 const ROOT_ELEMENT: HTMLElement = document.getElementById('root');
 
 const AppBody: React.FC = () => {
-    MovieService.getMovies({limit: 20, sortBy: 'title'});
-
     return (
         <AppCatch>
             <Provider store={store}>
                 <ThemeProvider theme={mainTheme}>
-                    <GlobalStyle/>
-                    <Routing/>
-                    <Footer/>
+                    <GlobalStyle />
+                    <Routing />
+                    <Footer />
                 </ThemeProvider>
             </Provider>
         </AppCatch>
@@ -30,8 +27,5 @@ const AppBody: React.FC = () => {
 };
 
 export const App: App = () => {
-    ReactDOM.render(
-        <AppBody/>,
-        ROOT_ELEMENT
-    );
+    ReactDOM.render(<AppBody />, ROOT_ELEMENT);
 };
--- home-work-app/src/app/interfaces/IQueryParams.d.ts
+++ home-work-app/src/app/interfaces/IQueryParams.d.ts
@@ -1,4 +1,5 @@
 export interface IQueryParams {
     sortBy: string;
+    limit?: number;
     filter?: string[];
 }
